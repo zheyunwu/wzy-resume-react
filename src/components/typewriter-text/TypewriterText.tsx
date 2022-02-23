60d480eb03a4180b35ba0c52b2d626035034486e
Zheyun Wu <wuzheyun@gmail.com>
2022-02-23 15:28:08 +0100

    Support pass in some parameters

--- src/components/typewriter-text/TypewriterText.tsx
+++ src/components/typewriter-text/TypewriterText.tsx
@@ -2,11 +2,17 @@ import React, { FC } from 'react';
 
 import './TypewriterText.scss';
 
-const TypewriterText: FC = ({children}) => {
+
+const TypewriterText: FC<{duration?: number, delay?: number, loop?: boolean, }> = ({children, duration=5, delay=0, loop=false}) => {
+  const styles = {
+    typingEffect: { 
+      animation: `typing ${duration}s steps(29,end) ${delay}s ${loop ? 'infinite' : '1'} normal both, blink-cursor 600ms steps(29,end) infinite`,
+    }
+  }
 
   return (
     <div className='typing-container'>
-      <div className='typing-wrapper'>
+      <div className='typing-wrapper' style={styles.typingEffect}>
         {children}
       </div>
     </div>
