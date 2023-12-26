0c295a673c65afe6c3c4f1bb96eb9fe2072dca3a
Zheyun <wuzheyun@gmail.com>
2023-12-26 20:43:55 +0800

    Add SAA certificate

--- src/pages/home/components/certificates/Certificates.tsx
+++ src/pages/home/components/certificates/Certificates.tsx
@@ -5,6 +5,7 @@ import ContentBlock from 'components/content-block/ContentBlock';
 
 import AWS_CLOUD_PRACTITIONER from 'assets/certificates/aws-cloud-practitioner.png';
 import AWS_DEVELOPER from 'assets/certificates/aws-developer.png';
+import AWS_SAA from 'assets/certificates/aws-solution-architect.png';
 
 const Certificates: FC = () => {
   return (
@@ -23,6 +24,12 @@ const Certificates: FC = () => {
             src={AWS_DEVELOPER}
             alt='AWS Developer'
             onClick={() => window.open('https://www.credly.com/badges/a559b987-6817-4ad3-aa09-b36bce859798/public_url')}
+          />
+          <img
+            className='certificates-item'
+            src={AWS_SAA}
+            alt='AWS Solution Architect'
+            onClick={() => window.open('https://www.credly.com/badges/cf36015c-0e81-41fc-bf5a-afe4c375c68f/public_url')}
           />
       </div>
     </ContentBlock>
